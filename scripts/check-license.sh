0400c50f81b321e69b421357b6b95568f8e3d1df
Harry Wagstaff <harry.wagstaff@gmail.com>
2018-06-19 11:15:14 +0100

    Add script to check for license on cpp, h, and S files

---
+++ scripts/check-license.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Check for the following text at the top of each source and header
+# file:
+
+# /* This file is Copyright University of Edinburgh 2018. For license details, see LICENSE. */
+
+FILES=`find . -regextype egrep -regex ".*/.*\.(h|cpp|S)"`
+BASEPATH=`pwd`
+
+ERROR=0
+
+for file in $FILES; do
+	SKIPCHECK=`grep -E "^/\* SKIP LICENSE CHECK \*/" $file | head -n1`
+	
+	if [ "$SKIPGUARDCHECK" != "" ]; then
+		continue
+	fi
+	
+	LICENSE="/* This file is Copyright University of Edinburgh 2018. For license details, see LICENSE. */"
+	FOUNDLICENSE=`grep -E "$LICENSE" $file | head -n1`
+	
+	if [ "$FOUNDLICENSE" = "" ]; then
+		LICENSEMISSING=1
+	else
+		LICENSEMISSING=0
+	fi
+	
+	if [ $LICENSEMISSING -eq 1 ]; then
+		ERROR=1
+		echo "$file"
+	fi
+	
+done
+
+exit $ERROR
