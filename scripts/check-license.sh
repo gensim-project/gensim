dd7bd3937d9fc5d31c61a92f7ad90e1e0e0a9492
Harry Wagstaff <fred.the.fish@gmail.com>
2018-06-20 11:39:20 +0100

    scripts: Improve scripts for license checking and reformatting, and add a precommit hook script

--- scripts/check-license.sh
+++ scripts/check-license.sh
@@ -5,11 +5,60 @@
 
 # /* This file is Copyright University of Edinburgh 2018. For license details, see LICENSE. */
 
-FILES=`find . -regextype egrep -regex ".*/.*\.(h|cpp|S)"`
+HGROOT=`hg root`
 BASEPATH=`pwd`
+
+# Parse options
+OPTIONS=mq
+LONGOPTIONS=modified-only,quiet
+QUIET=0
+
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+	exit 2
+fi
+
+eval set -- "$PARSED"
+
+while true; do
+	case "$1" in 
+		-q|--quiet)
+			QUIET=1
+			shift
+			;;
+		-m|--modified-only)
+			MODIFIED_ONLY=1
+			shift
+			;;
+		--)
+			shift
+			break;
+			;;
+		*)
+			echo "Error."
+			exit 3
+			;;
+	esac
+done
+
+function eecho()
+{
+	if [[ $QUIET -eq 0 ]]; then
+		echo $*
+	fi
+}
 
 ERROR=0
 
+# Figure out what files we need to check
+FILES=""
+if [ $MODIFIED_ONLY ]; then
+	FILES=$(hg st -man | grep -E "\.(cpp|h)$" | xargs -i echo $HGROOT/{})
+else
+	FILES=$(hg st -macn | grep -E "\.(cpp|h)$" | xargs -i echo $HGROOT/{})
+fi
+
+# For each file, grep for the license header (or skip if it should be skipped)
 for file in $FILES; do
 	SKIPCHECK=`grep -E "^/\* SKIP LICENSE CHECK \*/" $file | head -n1`
 	
@@ -28,7 +77,12 @@ for file in $FILES; do
 	
 	if [ $LICENSEMISSING -eq 1 ]; then
 		ERROR=1
-		echo "License missing in '$file'"
+		eecho "License missing in '$file'"
+		
+		# Don't need to keep looking if we're in quiet mode
+		if [ $QUIET -eq 1 ]; then
+			break;
+		fi;
 	fi
 	
 done
