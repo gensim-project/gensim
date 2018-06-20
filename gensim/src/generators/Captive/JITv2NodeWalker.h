985faa712a05c6f7eb8d50bd46680cd729f66552
Tom Spink <tspink@inf.ed.ac.uk>
2018-06-20 13:24:56 +0100

    Updates to the Captive generator, to support interface changes

--- gensim/src/generators/Captive/JITv2NodeWalker.h
+++ gensim/src/generators/Captive/JITv2NodeWalker.h
@@ -7,35 +7,31 @@
 
 #include "genC/ssa/SSAWalkerFactory.h"
 
-namespace gensim
-{
-	namespace genc
-	{
-		namespace ssa
-		{
-			class RegisterAllocation;
-		}
-	}
+namespace gensim {
+    namespace genc {
+        namespace ssa {
+            class RegisterAllocation;
+        }
+    }
 
-	namespace generator
-	{
-		namespace captive
-		{
-			class JITv2NodeWalkerFactory : public genc::ssa::SSAWalkerFactory
-			{
-			public:
-				JITv2NodeWalkerFactory(const genc::ssa::RegisterAllocation& ra) : _ra(ra) { }
+    namespace generator {
+        namespace captive {
 
-				genc::ssa::SSANodeWalker *Create(const genc::ssa::SSAStatement *statement) override;
+            class JITv2NodeWalkerFactory : public genc::ssa::SSAWalkerFactory {
+            public:
 
-				const genc::ssa::RegisterAllocation& RegisterAllocation() const
-				{
-					return _ra;
-				}
+                JITv2NodeWalkerFactory(const genc::ssa::RegisterAllocation& ra) : _ra(ra) {
+                }
 
-			private:
-				const genc::ssa::RegisterAllocation& _ra;
-			};
-		}
-	}
+                genc::ssa::SSANodeWalker *Create(const genc::ssa::SSAStatement *statement) override;
+
+                const genc::ssa::RegisterAllocation& RegisterAllocation() const {
+                    return _ra;
+                }
+
+            private:
+                const genc::ssa::RegisterAllocation& _ra;
+            };
+        }
+    }
 }
