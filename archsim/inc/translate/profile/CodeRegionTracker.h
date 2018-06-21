08aa54b2340e04cc63d6046ed7d5015a7426d3f2
Alberto Magni <alberto.magni@microsoft.com>
2018-06-21 17:51:27 +0100

    Speedup CodeRegionTracker::Invalidate
    Rather than resetting individual bits for each Page, reset the whole
    bitset.

--- archsim/inc/translate/profile/CodeRegionTracker.h
+++ archsim/inc/translate/profile/CodeRegionTracker.h
@@ -60,7 +60,7 @@ namespace archsim
 				}
 				void Invalidate()
 				{
-					for(uint32_t i = 0; i < RegionArch::PageCount; ++i) InvalidateRegion(PhysicalAddress(i << RegionArch::PageBits));
+					code_regions.reset();
 				}
 
 			private:
