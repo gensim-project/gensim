fd7926b700299dda0120545267d487f61d9f685a
Harry Wagstaff <harry.wagstaff@gmail.com>
2018-06-26 11:50:30 +0100

    CI: Update Jenkinsfiles to use new script, update script to use current user

--- scripts/dockerfiles/test-dockerfile.sh
+++ scripts/dockerfiles/test-dockerfile.sh
@@ -15,7 +15,7 @@ echo "Got image $IMAGEID"
 
 echo "Testing build..."
 
-docker run --rm -it -v $(hg root):/workspace $IMAGEID bash -c \
+docker run -u $(whoami) --rm -it -v $(hg root):/workspace $IMAGEID bash -c \
 	 "\
 	 cd /workspace && \
 	 mkdir -p $BUILDDIR && \
