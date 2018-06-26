88450e5b7a15fbd14f9c8bda036fa406b901aa55
Harry Wagstaff <fred.the.fish@gmail.com>
2018-06-26 13:29:10 +0100

    CI: Remove requirement for TTY when using test-dockerfile script

--- scripts/dockerfiles/test-dockerfile.sh
+++ scripts/dockerfiles/test-dockerfile.sh
@@ -15,7 +15,7 @@ echo "Got image $IMAGEID"
 
 echo "Testing build..."
 
-docker run -u $(id -u) --rm -it -v $(hg root):/workspace $IMAGEID bash -c \
+docker run -u $(id -u) --rm -v $(hg root):/workspace $IMAGEID bash -c \
 	 "\
 	 cd /workspace && \
 	 mkdir -p $BUILDDIR && \
