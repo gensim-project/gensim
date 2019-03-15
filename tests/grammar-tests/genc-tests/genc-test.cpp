b95e0e7a20e8e488674297d01db0e85b371a304d
Harry Wagstaff <harry.wagstaff@gmail.com>
2019-03-15 15:30:42 +0000

    Start working on new flex/bison based genC grammar

---
+++ tests/grammar-tests/genc-tests/genc-test.cpp
@@ -0,0 +1,31 @@
+/* This file is Copyright University of Edinburgh 2018. For license details, see LICENSE. */
+
+#include <flexbison_harness.h>
+#include <genC.tabs.h>
+#include <genC.l.h>
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 2) {
+		fprintf(stderr, "Usage: genc-test [input gc file]\n");
+		return 1;
+	}
+
+	FILE *f = fopen(argv[1], "r");
+
+	yyscan_t scanner;
+
+	yylex_init(&scanner);
+	yyset_in(f, scanner);
+
+	astnode<GenCNodeType> *root = CreateNode(GenCNodeType::ROOT);
+
+	yyparse(scanner, root);
+
+	yylex_destroy(scanner);
+
+	root->Dump();
+
+	fclose(f);
+}
