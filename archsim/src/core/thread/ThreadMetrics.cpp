96f87d25fd6c87cc29f152394419a5c9af7a2eba
Harry Wagstaff <harry.wagstaff@gmail.com>
2019-03-27 15:07:40 +0000

    archsim: Fix metrics in blockjit, work on llvm jit.

--- archsim/src/core/thread/ThreadMetrics.cpp
+++ archsim/src/core/thread/ThreadMetrics.cpp
@@ -63,8 +63,13 @@ void ThreadMetricPrinter::PrintStats(const ArchDescriptor &arch, const ThreadMet
 	str << "Infrastructure Runtime: " << metrics.SelfRuntime.GetElapsedS() - (metrics.InterpretTime.GetElapsedS() + metrics.JITTime.GetElapsedS()) << " seconds" << std::endl;
 	str << "Execution Rate: " << (metrics.InstructionCount.get_value() / 1000000.0) / metrics.SelfRuntime.GetElapsedS() << " MIPS" << std::endl;
 
-	str << "JIT Rate: " << (metrics.JITInstructionCount.get_value() / 1000000.0) / metrics.JITTime.GetElapsedS() << " MIPS" << std::endl;
-	str << "Interpreter Rate: " << ((metrics.InstructionCount.get_value() - metrics.JITInstructionCount.get_value()) / 1000000.0) / (metrics.InterpretTime.GetElapsedS()) << " MIPS" << std::endl;
+	if(metrics.JITTime.GetElapsedS() != 0) {
+		str << "JIT Rate: " << (metrics.JITInstructionCount.get_value() / 1000000.0) / metrics.JITTime.GetElapsedS() << " MIPS" << std::endl;
+	}
+
+	if(metrics.InterpretTime.GetElapsedS() != 0) {
+		str << "Interpreter Rate: " << ((metrics.InstructionCount.get_value() - metrics.JITInstructionCount.get_value()) / 1000000.0) / (metrics.InterpretTime.GetElapsedS()) << " MIPS" << std::endl;
+	}
 
 	str << "Successful chains: " << metrics.JITSuccessfulChains.get_value() << std::endl;
 	str << "Failed chains: " << metrics.JITFailedChains.get_value() << std::endl;
